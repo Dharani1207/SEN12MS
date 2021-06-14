1dea2bf853510fea15bce00942d9256c8b4f8c54
Dharani1207 <dharanideivasihamani@gmail.com>
2021-06-14 14:18:51 +0200

    To run customscript on entire dataset

--- classification/customScript.py
+++ classification/customScript.py
@@ -7,11 +7,16 @@ import rasterio
 
 #SEN12MS Class - Data Generating class
 class SEN12MS(data.Dataset):
-    def __init__(self):
+    def __init__(self,cloudy_dataset = False, localComputer = True):
         
        # data path
-        data_dir = "SEN12MS\data"               # SEN12MS dir
-        list_dir = "SEN12MS\label_split_dir"    # split lists/ label dirs
+        if localComputer:
+            data_dir = "SEN12MS\data"               # SEN12MS dir
+            list_dir = "SEN12MS\label_split_dir"    # split lists/ label dirs
+        else:
+            data_dir = "/media/dharani/SpeicherTUM/patrickebel/SEN12MS/m1474000"
+            list_dir = "/home/dharani/Desktop/SEN12MS/label_split_dir"    # split lists/ label dirs
+    
    
         # For now, since we are using only S2 data in local computer, these flags make no sense
         # self.use_s2 = True
@@ -26,13 +31,13 @@ class SEN12MS(data.Dataset):
             self.n_classes = 17 
         
         #Loading the labels
-        label_file_path = "SEN12MS\\label_split_dir\\IGBP_probability_labels.pkl"
+        label_file_path = "SEN12MS\\label_split_dir\\IGBP_probability_labels.pkl" #Local : "SEN12MS\\label_split_dir\\IGBP_probability_labels.pkl" os.path.join(list_dir,"IGBP_probability_labels.pkl")
         label_file = open(label_file_path, "rb")
         self.labels = pkl.load(label_file)
         label_file.close()
         
         #Loading the samples
-        samples_file_path = "SEN12MS\\label_split_dir\\val_list.pkl"
+        samples_file_path = "SEN12MS\\label_split_dir\\train_list.pkl" #Local : "SEN12MS\\label_split_dir\\val_list.pkl" os.path.join(list_dir,"val_list.pkl")
         sample_file = open(samples_file_path, "rb")
         self.samples =  pkl.load(sample_file)
         sample_file.close()
@@ -41,11 +46,14 @@ class SEN12MS(data.Dataset):
         self.sampleObject = []    
         for s2_id in self.samples:
             mini_name = s2_id.split("_")
-            s2_loc = os.path.join(data_dir, (mini_name[0]+'_'+mini_name[1]),(mini_name[2]+'_'+mini_name[3]), s2_id)
+            if cloudy_dataset:
+                s2_loc = os.path.join(data_dir, (mini_name[0]+'_'+mini_name[1] +'_'+ mini_name[2]+'_'+mini_name[3]),(mini_name[2]+'_'+mini_name[3] + '_'+ mini_name[4]), s2_id)
+            else:
+                s2_loc = os.path.join(data_dir, (mini_name[0]+'_'+ mini_name[1]), (mini_name[2]+'_'+mini_name[3]), s2_id)
             s1_loc = s2_loc.replace("_s2_", "_s1_").replace("s2_", "s1_")
             self.sampleObject.append({"s1": s1_loc, "s2": s2_loc, "id": s2_id})
        
-    def __getitem__(self, index,use_all):
+    def __getitem__(self, index,use_all = True):
         #Bands to read with
         S2_BANDS_LD = [2, 3, 4, 5, 6, 7, 8, 9, 12, 13]
         S2_BANDS_RGB = [2, 3, 4] # B(2),G(3),R(4)
@@ -60,7 +68,7 @@ class SEN12MS(data.Dataset):
         # load label
         lc = self.labels[self.samples[index]]
     
-        # covert label to IGBP simplified scheme
+        # convert label to IGBP simplified scheme
         cls1 = sum(lc[0:5]);
         cls2 = sum(lc[5:7]); 
         cls3 = sum(lc[7:9]);
